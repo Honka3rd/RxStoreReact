c0a8b1e86e09d4357804c88818681d6f3fe7a227
Honka3rd <ak47m40080@gmail.com>
2023-09-13 15:45:54 +0800

    fix typed issue

--- dist/factories/createObservableStates.d.ts
+++ dist/factories/createObservableStates.d.ts
@@ -1,4 +1,4 @@
 import { Record } from "immutable";
 import { BS, IBS, RxImStore, RxNStore, Subscribable, ConstraintKeys } from "rx-store-types";
-export declare const createObservableNormalStates: <S extends BS>(store: RxNStore<S> & Subscribable<S>) => <T extends keyof S>(keys: ConstraintKeys<T>) => { [k in T]: ReturnType<S[k]>; };
-export declare const createObservableImmutableStates: <S extends IBS>(store: RxImStore<S> & Subscribable<S>) => <T extends keyof S>(keys: ConstraintKeys<T>) => Record<{ [K in T]: ReturnType<S[K]>; }> & Readonly<{ [K in T]: ReturnType<S[K]>; }>;
+export declare const createObservableNormalStates: <S extends BS>(store: RxNStore<S> & Subscribable<S>) => <T extends keyof S>(keys: readonly T[]) => { [k in T]: ReturnType<S[k]>; };
+export declare const createObservableImmutableStates: <S extends IBS>(store: RxImStore<S> & Subscribable<S>) => <T extends keyof S>(keys: readonly T[]) => Record<{ [K in T]: ReturnType<S[K]>; }> & Readonly<{ [K in T]: ReturnType<S[K]>; }>;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,7 +3,7 @@ import { RxNStoreImpl } from "rx-store-core/dist/main/normal";
 import { RxImStoreImpl } from "rx-store-core/dist/main/immutable";
 declare const stateObserverManager: <S extends BS>(store: RxNStoreImpl<S>) => {
     useObservableState: <T extends keyof S>(key: T) => readonly [ReturnType<S[T]>, (val: ReturnType<S[T]>) => void];
-    useObservableStates: <T_1 extends keyof S>(keys: import("rx-store-types").ConstraintKeys<T_1, T_1, T_1>) => { [k in T_1]: ReturnType<S[k]>; };
+    useObservableStates: <T_1 extends keyof S>(keys: readonly T_1[]) => { [k in T_1]: ReturnType<S[k]>; };
     useObservableSelector: <R>(computation: import("rx-store-types").Computation<R, S>) => NonNullable<R>;
     useObservableReducer: <K extends keyof S, T_2 extends string>(key: K, reducer: import("rx-store-types").Reducer<T_2, S, K>) => readonly [ReturnType<S[K]>, import("rx-store-types").Dispatch<ReturnType<S[K]>, T_2>];
     useObservableAsyncComputation: <R_1>(computation: import("rx-store-types").ComputationAsync<R_1, S>, fallback: R_1, comparator?: import("rx-store-types").Comparator<{ [K_1 in keyof S]: ReturnType<S[K_1]>; }> | undefined) => import("./interfaces").AsyncMetaStates<R_1>;
@@ -11,7 +11,7 @@ declare const stateObserverManager: <S extends BS>(store: RxNStoreImpl<S>) => {
 };
 declare const immutableStateObserverManager: <S extends IBS>(store: RxImStoreImpl<S>) => {
     useImmutableObservableState: <T extends keyof S>(key: T) => readonly [ReturnType<S[T]>, (val: ReturnType<S[T]>) => void];
-    useImmutableObservableStates: <T_1 extends keyof S>(keys: import("rx-store-types").ConstraintKeys<T_1, T_1, T_1>) => import("immutable").Record<{ [K in T_1]: ReturnType<S[K]>; }> & Readonly<{ [K in T_1]: ReturnType<S[K]>; }>;
+    useImmutableObservableStates: <T_1 extends keyof S>(keys: readonly T_1[]) => import("immutable").Record<{ [K in T_1]: ReturnType<S[K]>; }> & Readonly<{ [K in T_1]: ReturnType<S[K]>; }>;
     useImmutableObservableSelector: <R>(computation: import("rx-store-types").Computation<R, S>) => NonNullable<R>;
     useImmutableObservableReducer: <K_1 extends keyof S, T_2 extends string>(key: K_1, reducer: import("rx-store-types").Reducer<T_2, S, K_1>) => readonly [ReturnType<S[K_1]>, import("rx-store-types").Dispatch<ReturnType<S[K_1]>, T_2>];
     useImmutableObservableAsyncComputation: <R_1>(computation: import("rx-store-types").ComputationAsync<R_1, S>, fallback: R_1, comparator?: import("rx-store-types").Comparator<{ [K_2 in keyof S]: ReturnType<S[K_2]>; }> | undefined) => import("./interfaces").AsyncMetaStates<R_1>;
